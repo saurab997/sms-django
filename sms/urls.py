a5f122ffda57ff69e22fc6a805063b91034ff238
Saurab <dhakalsaurab1234@gmail.com>
2026-07-04 16:11:36 +0545

    path declared

--- sms/urls.py
+++ sms/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('create/', create),
+    path('update/<id>', update),
+    path('delete/<id>', delete),
 ]
